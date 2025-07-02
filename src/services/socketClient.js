4e525a28a456634f03264d3ab7fdf0f27acecb18
YAOP77 <yaop77@gmail.com>
2025-07-02 11:14:37 +0000

    react-uploads

--- src/services/socketClient.js
+++ src/services/socketClient.js
@@ -1,9 +1,11 @@
-// socketClient.js
-import { io } from 'socket.io-client';
-const API_URL = process.env.REACT_APP_API_URL;
+import { io } from "socket.io-client";
 
-const socket = io(API_URL, {
+const API_URL = process.env.REACT_APP_API_URL || "https://express-linko.onrender.com";
+const SOCKET_URL = API_URL.replace(/\/api$/, ''); // Enlève /api si présent
+
+const socket = io(SOCKET_URL, {
   transports: ['websocket'],
+  withCredentials: true,
 });
 
 export default socket;
